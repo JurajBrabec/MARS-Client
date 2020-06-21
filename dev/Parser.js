02267fc5256c33b3bd7d302e8f154ca4a107514c
Juraj Brabec <juraj@brabec.sk>
2020-06-21 19:20:02 +0200

    Parser

---
+++ dev/Parser.js
@@ -0,0 +1,255 @@
+const debug = require("debug")("Parser");
+
+delimiter = "\n";
+escapeChar = "\\";
+quoteChar = '"';
+separator = ",";
+
+_actions = [];
+_buffer = "";
+_items = [];
+
+function _iterate(item, func = {}) {
+  func = { ...{ text: (t) => t, row: (r) => r }, ...func };
+  let result;
+  if (_isText(item)) {
+    result = func.text(item);
+  } else if (_hasText(item)) {
+    result = item.map((item) => _iterate(item, func));
+    result = func.row(result);
+  } else {
+    result = item
+      .map((item) => _iterate(item, func))
+      .filter((row) => row.length);
+  }
+  return result;
+}
+function _action(result, action, index) {
+  debug("action", index + 1, action);
+  return Object.entries(action).reduce((result, [name, param], index) => {
+    debug("part", index + 1, name);
+    let e;
+    let p;
+    let q;
+    let r1;
+    let r2;
+    let s;
+    const func = {};
+    switch (name) {
+      case "expect":
+        if (typeof param === "number") {
+          func.row = (row) =>
+            row.length === param
+              ? row
+              : _error(`Expected ${param} strings got ${row.length}`);
+        } else {
+          func.text = (text) =>
+            _match(text, param)
+              ? text
+              : _error(`Expected text "${param}" got "${text}"`);
+        }
+        break;
+      case "filter":
+        func.row = (row) => row.filter((text) => !_match(param, text));
+        break;
+      case "lcase":
+        func.text = (text) => text.toLowerCase();
+        break;
+      case "pop":
+        func.row = (row) => {
+          row.pop();
+          return row.length === 1 ? row[0] : row;
+        };
+        break;
+      case "quote":
+        q = param;
+        r1 = new RegExp(`^${q}`);
+        r2 = new RegExp(`${q}$`);
+        func.text = (text) =>
+          (r1.test(text) ? "" : q) + text + (r2.test(text) ? "" : q);
+        break;
+      case "reject":
+        func.text = (text) =>
+          _match(text, param)
+            ? _error(`Rejecting text "${param}" found`)
+            : text;
+        break;
+      case "replace":
+        func.text = (text) => text.replace(...param);
+        break;
+      case "separate":
+        e = escapeChar;
+        q = quoteChar;
+        s = param;
+        p = `(${q}.*?${q}|(?:\\${e + s}|[^${q + s}])+|(?<=${s}))(?=${s}|$)`;
+        r1 = new RegExp(p, "gm");
+        func.text = (text) => text.match(r1);
+        break;
+      case "shift":
+        func.row = (row) => {
+          row.shift();
+          return row.length === 1 ? row[0] : row;
+        };
+        break;
+      case "split":
+        func.text = (text) => text.split(param);
+        break;
+      case "ucase":
+        func.text = (text) => text.toUpperCase();
+        break;
+      case "unpivot":
+        func.row = (row) => {
+          firstText = row.shift();
+          return row.map((text) => firstText + separator + text);
+        };
+        break;
+      case "unquote":
+        q = param;
+        r1 = new RegExp(`^${q}|${q}$`, "g");
+        func.text = (text) => text.replace(r1, "");
+        break;
+      case "trim":
+        r1 = undefined;
+        if (param instanceof RegExp) param = param.source;
+        if (param) r1 = new RegExp(`^${param}+|${param}+$`, "gm");
+        func.text = r1 ? (text) => text.replace(r1, "") : (text) => text.trim();
+        break;
+      default:
+        break;
+    }
+    result = _iterate(result, func);
+    debug(result);
+    return result;
+  }, result);
+}
+function _addAction(action) {
+  debug("addAction", _actions.length, action);
+  _actions.push(action);
+}
+_hasText = (v) => _isText(v[0]);
+_isText = (v) => !Array.isArray(v);
+_match = (v, m) => (m instanceof RegExp ? m.test(v) : v === m);
+function _error(error) {
+  throw error;
+}
+function buffer(text) {
+  debug("buffer", text);
+  _buffer += text;
+  action1 = _actions.find((action) => action.split || action.separate);
+  const r = action1.split || action1.separate;
+  if (!r.test(_buffer)) return;
+  const newBuffer = _buffer.split(r).pop();
+  text = _buffer.slice(0, -newBuffer.length);
+  _buffer = newBuffer;
+  return JSON.stringify(_actions.reduce(_action, text));
+}
+function debugEnabled(enabled = true) {
+  debug.enabled = enabled;
+  return this;
+}
+function delimited(delimited) {
+  delimiter = delimited;
+  return this;
+}
+function end() {
+  debug("end");
+  return JSON.stringify(_actions.reduce(_action, _buffer));
+}
+function escaped(escaped) {
+  escapeChar = escaped;
+  return this;
+}
+function expect(expected) {
+  _addAction({ expect: expected });
+  return this;
+}
+function filter(filtered = "") {
+  _addAction({ filter: filtered });
+  return this;
+}
+function lcase() {
+  _addAction({ lcase: null });
+  return this;
+}
+function parse(text) {
+  debug("parse", text);
+  return JSON.stringify(_actions.reduce(_action, text));
+}
+function pop() {
+  _addAction({ pop: null });
+  return this;
+}
+function quoted(quoted) {
+  quoteChar = quoted;
+  return this;
+}
+function quote(quoted = quoteChar) {
+  _addAction({ quote: quoted });
+  return this;
+}
+function reject(rejected) {
+  _addAction({ reject: rejected });
+  return this;
+}
+function replace(found, replaced = "") {
+  _addAction({ replace: [found, replaced] });
+  return this;
+}
+function shift() {
+  _addAction({ shift: null });
+  return this;
+}
+function separate(separated = separator) {
+  _addAction({ separate: separated, trim: null, filter: "" });
+  return this;
+}
+function separated(separated) {
+  separator = separated;
+  return this;
+}
+function split(delimited = delimiter) {
+  _addAction({ split: delimited, trim: null, filter: "" });
+  return this;
+}
+function ucase() {
+  _addAction({ ucase: null });
+  return this;
+}
+function unpivot() {
+  _addAction({ unpivot: null });
+  return this;
+}
+function unquote(quoted = quoteChar) {
+  quoteChar = quoted;
+  _addAction({ unquote: quoted, trim: null });
+  return this;
+}
+function trim(trimmed) {
+  _addAction({ trim: trimmed });
+  return this;
+}
+
+module.exports = {
+  buffer,
+  debug: debugEnabled,
+  delimited,
+  end,
+  escaped,
+  expect,
+  filter,
+  lcase,
+  parse,
+  pop,
+  quote,
+  quoted,
+  reject,
+  replace,
+  separate,
+  separated,
+  shift,
+  split,
+  ucase,
+  unpivot,
+  unquote,
+  trim,
+};
--- test-dev.js
+++ test-dev.js
@@ -1,20 +1,40 @@
-const Readable = require("./dev/Readable");
+const dotenv = require("dotenv").config();
+EmitterProcess = require("./dev/EmitterProcess");
+const parser = require("./dev/Parser");
+const path = require("path");
 
-const command = new Readable({
+const command = new EmitterProcess({
   debug: true,
-  read(size) {
-    this.push("data");
-    this.push(null);
-  },
+  args: ["-summary", "-l"],
+  file: path.join(process.env.NBU_BIN, "admincmd", "bpdbjobs.exe"),
 });
 command
-  .on("data", (data) => console.log(">Data:", data))
+  .on("data", onData)
   .once("error", (error) => console.log(">Error:", error.message || error))
   .once("end", (status) => console.log(">End", status))
   .on("progress", (progress) => console.log(">Progress:", progress));
 //  .pipe(process.stdout);
 command
   .run()
-  .then((result) => console.log("Result:", result))
+  .then((result) => console.log("Result:", parser.parse(result)))
   .catch((error) => console.log("Error:", error));
 //  .finally(() => database.end());
+
+parser
+  .debug(command.debug)
+  .split(/(\r?\n){2}/)
+  .filter()
+  .expect(/^Summary/)
+  .split(/\r?\n/)
+  .replace("on", ":")
+  .separate(":")
+  .shift()
+  .expect(10);
+function onData(data) {
+  try {
+    data = parser.parse(data);
+  } catch (error) {
+    console.log("Parsing error:", error);
+  }
+  console.log(">Data:", data);
+}
