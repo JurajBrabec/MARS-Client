5744f7b4bfb8c9910d0fbeeff86a03549e82f985
Juraj Brabec <juraj@brabec.sk>
2020-07-07 18:44:22 +0200

    TextParser

--- dev/nbu/bpretlevel.js
+++ dev/nbu/bpretlevel.js
@@ -1,27 +1,32 @@
-const path = require("path");
+const path = require('path');
+const { Actions } = require('../TextParser');
+const { Column, Row, Set } = Actions;
 
 class Retlevels {
   constructor(nbu) {
     this.process = {
-      file: path.join(nbu.bin, "admincmd", "bpretlevel.exe"),
-      args: ["-L"],
+      file: path.join(nbu.bin, 'admincmd', 'bpretlevel.exe'),
+      args: ['-L'],
     };
     this.parser = [
-      { split: /\r?\n/ },
-      { filter: "" },
-      { shift: 4 },
-      { expect: /^\d+/ },
-      { replace: [/\(|\)/g, ""] },
-      { split: / (?=\d+|expires|infinite)/ },
-      { expect: 4 },
+      Set.delimiter(/\r?\n/),
+      Column.trim(),
+      Column.expect(/^Retention/),
+      Row.split(),
+      Column.trim(),
+      Column.filter(/^\D/),
+      Column.replace([/\(|\)/g, '']),
+      Column.split(/ (?=\d+|expires|infinite)/),
+      Column.trim(),
+      Row.expect(4),
     ];
     this.tables = {
       bpretlevel: [
         { masterServer: nbu.masterServer, key: true },
-        { level: "number", key: true },
-        { days: "number" },
-        { seconds: "number" },
-        { period: "string" },
+        { level: 'number', key: true },
+        { days: 'number' },
+        { seconds: 'number' },
+        { period: 'string' },
       ],
     };
   }
--- dev/nbu/nbdevquery.js
+++ dev/nbu/nbdevquery.js
@@ -1,36 +1,39 @@
-const path = require("path");
+const path = require('path');
+const { Actions } = require('../TextParser');
+const { Column, Row, Set } = Actions;
 
 class PureDisks {
   constructor(nbu) {
     this.process = {
-      file: path.join(nbu.bin, "admincmd", "nbdevquery.exe"),
-      args: ["-listdv", "-stype", "PureDisk", "-l"],
+      file: path.join(nbu.bin, 'admincmd', 'nbdevquery.exe'),
+      args: ['-listdv', '-stype', 'PureDisk', '-l'],
     };
     this.parser = [
-      { split: /\r?\n/ },
-      { filter: "" },
-      { expect: /^V_\d/ },
-      { split: " " },
-      { expect: 15 },
+      Set.delimiter(/\r?\n/),
+      Column.expect(/^V_\d/),
+      Row.split(),
+      Column.filter(''),
+      Column.split(' '),
+      Row.expect(15),
     ];
     this.tables = {
       nbdevquery_listdv_puredisk: [
         { masterServer: nbu.masterServer, key: true },
-        { version: "string" },
-        { diskPool: "string" },
-        { sType: "string" },
-        { name: "string", key: true },
-        { disk_media_id: "string" },
-        { total_capacity: "float" },
-        { free_space: "float" },
-        { used: "number" },
-        { nbu_state: "number" },
-        { sts_state: "number" },
-        { num_write_mounts: "number" },
-        { active_read_streams: "number" },
-        { active_write_streams: "number" },
-        { flags: "number" },
-        { num_read_mounts: "number" },
+        { version: 'string' },
+        { diskPool: 'string' },
+        { sType: 'string' },
+        { name: 'string', key: true },
+        { disk_media_id: 'string' },
+        { total_capacity: 'float' },
+        { free_space: 'float' },
+        { used: 'number' },
+        { nbu_state: 'number' },
+        { sts_state: 'number' },
+        { num_write_mounts: 'number' },
+        { active_read_streams: 'number' },
+        { active_write_streams: 'number' },
+        { flags: 'number' },
+        { num_read_mounts: 'number' },
         { updated: nbu.startTime },
         { obsoleted: null },
       ],
