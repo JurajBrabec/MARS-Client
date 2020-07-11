a4fbcd4dc033bd5a62bac27d9492c1eda075e89c
Juraj Brabec <juraj@brabec.sk>
2020-07-11 10:46:42 +0200

    TextParser

--- dev/nbu/vaults.js
+++ dev/nbu/vaults.js
@@ -1,14 +1,14 @@
-const { Nbu } = require("./nbu");
+const { Nbu } = require('./nbu');
 
 const nbu = new Nbu();
 nbu
-  .vaults({ debug: true })
+  .vaults()
   .then((command) =>
     command
-      //      .asBatch()
+      .asBatch()
       //  .on("data", (data) => console.log(data))
       .run()
   )
   .then((objects) => console.log(objects))
   .catch((error) => console.log(error))
-  .finally(() => console.log("Done"));
+  .finally(() => console.log('Done'));
