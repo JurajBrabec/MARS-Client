6b7b963c6eee9c1bb4fb66c779d35c51940725df
Juraj Brabec <juraj@brabec.sk>
2020-07-12 10:57:35 +0200

    test

--- test-dev.js
+++ test-dev.js
@@ -1,49 +1,18 @@
 const fs = require('fs');
 const { Nbu } = require('./dev/nbu/nbu');
-const { SLPs } = require('./dev/nbu/nbstl');
+const { Policies } = require('./dev/nbu/bppllist');
 const { Actions, Parser } = require('./dev/TextParser');
 const tables = require('./lib/Tables');
 
 const nbu = new Nbu();
-const command = new SLPs(nbu);
+const command = new Policies(nbu);
 tables.create(command.tables);
 
 let text = fs.readFileSync(
-  'D:\\Veritas\\Netbackup\\bin\\admincmd\\nbstl\\l.txt',
+  'D:\\Veritas\\Netbackup\\bin\\admincmd\\bppllist\\allpolicies.txt',
   { encoding: 'utf8' }
 );
-const { Row, Set } = Actions;
-const actions = [
-  Set.delimiter(/\r?\n(?=[A-Za-z]+)/),
-  Set.separator(' '),
-  Set.external((text) => [
-    text.split(/\r?\n(?=[A-Za-z]+)/).reduce(
-      (array, text) => [
-        ...array,
-        ...text
-          .split(/\r?\n/)
-          .map((item) => item.trim())
-          .filter((item) => item.length)
-          .reduce((array, line, index) => {
-            if (index) {
-              array.push(
-                [firstLine, line]
-                  .join(' ')
-                  .split(' ')
-                  .map((item) => (item === '*NULL*' ? null : item))
-              );
-            } else {
-              firstLine = line;
-            }
-            return array;
-          }, []),
-      ],
-      []
-    ),
-  ]),
-  Row.expect(21),
-];
-const parser = new Parser(actions).debug();
+const parser = new Parser(command.parser).debug();
 const result = parser.parseText(text);
 console.log(tables.fromParser(result));
 
