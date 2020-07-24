5c0f4e55916c378585505ae53447478a56bbc691
Juraj Brabec <juraj@brabec.sk>
2020-07-24 12:32:53 +0200

    rollup

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: './mars.js',
+  output: {
+    file: './build/mars.min.js',
+    format: 'cjs',
+    name: 'mars',
+  },
+  plugins: [resolve(), commonjs(), json(), terser()],
+};
